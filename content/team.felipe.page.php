fad0af76cef9bcc0181508243c5eeae0fe3f4967
bpcampbe <bpcampbe@web.nmsu.edu>
2016-06-06 08:40:35 -0600

    Add Felipes resume

--- content/team.felipe.page.php
+++ content/team.felipe.page.php
@@ -8,7 +8,55 @@ echo <<<'EOD'
 							<!-- Content -->
 								<article>
 
-									<h2 style="text-align:center;">Coming Soon</h2>
+									
+<h2>Felipe Parra</h2>
+
+<h3>OBJECTIVE</h3>
+<p>To obtain a position as a System Administration Manager that will utilize my Windows system administration and troubleshooting skills and experience, with the opportunity to manage and mentor system analysts and desktop technicians.</p>
+<h3>EDUCATION</h3>
+<p>Bachelor of Business Administration, Major: Business Computer Systems<br>
+New Mexico State University, Las Cruces NM, December 2004, GPA: 3.14 </p>
+
+<h3>SKILLS</h3>
+<ul>
+    <li>Experience with the installation and administration of many different Windows server operating systems</li>
+    <li>Knowledge of many different programming languages including:  Java, HTML, PHP, CSS, and Shell Scripting</li>
+    <li>Experience installing and supporting Windows 7, Windows 10, Mac OS X and Linux based operating systems </li>
+    <li>Experience in database design and report creation with a strong knowledge of SQL and Oracle databases</li>
+    <li>Strong knowledge of TCP/IP networking</li>
+    <li>Able to communicate effectively both orally and written</li>
+</ul>
+
+<h3>WORK EXPERIENCE</h3>
+<p><STRONG>System Administration, Manager</STRONG> New Mexico State University, Las Cruces NM 3/07 &ndash; Present</p>
+<ul>
+    <li>Administer user accounts and security for all users within our area</li>
+    <li>Maintain Hobsons Connect and Hobsons Apply and serve as primary contact for NMSU with vendor and implement new functionality </li>
+    <li>Make recommendations to purchase hardware and software for desktops as well as servers</li>
+    <li>Create new web pages using PHP, HTML and JavaScript, as needed, and maintain and update existing pages</li>
+    <li>Develop ad hoc databases with web interfaces as needed for special events</li>
+    <li>Analyze current system and functional processes and assist with troubleshooting</li>
+    <li>Provide desktop support for multiple offices</li>
+    <li>Maintain inventory of computer equipment</li>
+    <li>Recommend computer upgrades or purchases</li>
+</ul>
+
+<p><strong>Computer Technical Support Analyst,</strong> Lockheed Martin, Albuquerque NM 8/05 &ndash; 3/07</p>
+<ul>
+    <li>Senior Macintosh Technician</li>
+     <li>Provided technical and training support to users either by telephone or on-site relative to desktop hardware and software packages</li>
+     <li>Performed hardware diagnostics and repairs</li>
+     <li>Installed and tested computers, printers, and other peripherals, configured operating systems, shrink-wrap programs, and applications software programs</li>
+     <li>Supported Apple computers running OS X and parallels</li>
+    <li>Developed reports and databases</li>
+</ul>
+<p><strong>Computer Technician,</strong> Priority One Computer Solutions, Albuquerque NM 2/05 &ndash; 8/05 </p>
+<ul>
+<li>Provided desktop technical support and counsel to many small firms throughout Albuquerque and Santa Fe, New Mexico
+    </li>
+    <li>Helped maintain company networks and servers for many of the same firms </li>
+    <li>Selected and installed desktop and server operating systems for clients as needed </li>
+</ul>
 							</article>
 
 						</div>
