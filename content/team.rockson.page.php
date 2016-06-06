81ac1bf39f29067739adcda08d4a77145ae62c16
bpcampbe <bpcampbe@web.nmsu.edu>
2016-06-05 23:00:00 -0600

    Add Rockson's resume

--- content/team.rockson.page.php
+++ content/team.rockson.page.php
@@ -1,16 +1,125 @@
 <?php
 echo <<<'EOD'
-<!-- Main -->
+			<!-- Main -->
 				<div id="main-wrapper">
 					<div class="container">
-						<div id="content">
+						<div class="row 200%">
+							<div class="4u 12u$(medium)">
+								<div id="sidebar">
 
-							<!-- Content -->
-								<article>
+									<!-- Sidebar -->
+										<section>
+																							    1805 Bellamah Drive 
+	<br>1980 Horal Dr 
+	<br>San Antonio, Texas 78440 
+	<br>+1.830.308.2392 
+	<br>rocksonopk@gmail.com
 
-									<h2 style="text-align:center;">Coming Soon</h2>
-							</article>
+											<footer>
+												<a href="#" class="button icon fa-info-circle">Contact Rockson</a>
+											</footer>
+										</section>
 
+
+								</div>
+							</div>
+							<div class="8u 12u$(medium) important(medium)">
+								<div id="content">
+
+									<!-- Content -->
+										<article>
+<h2>
+Rockson Opoku
+</h2>
+<p>PROFILE: Highly organized and detail oriented Business Analyst with a solid background in IT and business environment. Accomplished ERP (SAP / Maximo) professional with proven ability to multi task, enhanced with exceptional organizational skills. Possess excellent planning and prioritizing skills coupled with strong problem-solving skills and the ability to make independent decisions, excellent ability to motivate others. Communicate comfortably and efficiently at all levels, therefore ensuring an excellent working environment for all staff members. Adept at defining business needs and translating them into cutting edge designs and products that strengthen work processes and improve operational efficiency.</p>
+
+<h3>
+Skills Highlight
+</h3>
+<ul>
+<li>Over 4 years of experience in delivering unbeatable customer service</li>
+<li>Skill in  SAP ECC6</li>
+<li>Knowledge of SAP modules HR, FI, PR, MM etc</li>
+<li>Knowledge of infoProviders, MultiProviders, infoCubes, ETL.</li>
+<li>Ability to learn and adapt to change</li>
+<li>Networking LAN, WLAN</li>
+<li>Troubleshooting and configuring Printers</li>
+<li>Efficient in Microsoft Office (Access, Excel, Word, and PowerPoint) and personal computer skills</li>
+<li>Proficient in data entry, maintaining records, filing and/or retrieving documents</li>
+<li>Easily adaptable to all working environments and a great team player</li>
+<li>Communicates effectively oral and written</li>
+<li>Experience in IT Audit and system support</li>
+<li>Proficient in assessing system/network availability, security, and data integrity to identify, manage, and reduce risks, and ensure general compliance.</li>
+<li>Strong background in several stages of audits, including planning; study, evaluation, and testing controls; reporting; and follow-up.</li>
+<li>Knowledge of SOX and business processes</li>
+<li>Knowledge and understanding of networking, hardware, software, and data centers.</li>
+</ul>
+<h4>
+City Of San Antonio
+<br>ITSD, Customer Support Analyst 02/17/2016 to Present  
+</h4>
+<ul>
+<li>Assist users and clients on Mainframe issues (Password reset, reset session ect.).</li> 
+<li>Respond to CRQ's to assign roles, add or terminate users in Active Directory</li>
+<li>Serve as a liaison between SAP users and the ITSD in order to provide technical solutions to meet user needs.</li>
+<li>Assist users with SAP related issues in clients (550, 502 etc) environments</li>
+<li>Conducted SAP and legacy system reviews to support products and ensure business objectives are met</li>
+<li>Assist users via Bomgar session to troubleshoot issues</li>
+<li>Diagnose escalated customer problems and perform root cause analysis to address typical problems.</li>
+<li>Research trouble issues and assist in the resolution of application, hardware, and software problems.</li>
+<li>System Support</li>
+<li>Document and coordinate updates on issues to ensure customer satisfaction and productivity.</li>
+<li>Work with vendor technical support personnel on solutions for customers.</li>
+</ul>
+<h4>
+Cord Blood Registry Tucson, AZ 
+<br>Customer Service Representative 08/2014 to 04/2015 
+</h4>
+<ul>
+<li>Word processing manuscripts, letters, and documents</li>
+<li>Contributed to and maintained system standards</li>
+<li>Performed daily system monitoring, reviewing system and application logs, and verifying of scheduled jobs such as backups</li>
+<li>Answering phone calls and forwarding callers to appropriate staff members</li>
+<li>Created, changed, and deleted user accounts per request</li>
+<li>Performed regular security monitoring to identify any possible intrusions</li>
+<li>Performed regular file archival and purge as necessary</li>
+<li>Typed and filed memos and correspondence</li>
+<li>Scheduled appointments and meetings for clients and management.</li>
+</ul>
+<h4>
+AFNI Tucson, AZ 
+<br>Senior Sales Associate 08/2013 to 07/2014
+</h4>
+<ul>
+<li>Collected and analyzed customer based data to determine customer demographics, purchasing habits and best sellers, resulting in better pricing recommendation to management.</li>
+<li>Co-led the development and implementation of simplified membership sales, improving individual Associates by 125% in less than a month, using IT customized sales tools.</li>
+<li>Maintained a customer satisfaction rating of 100% during tenure.</li>
+<li>Extensively used call center and online data logs to identify channel deficiencies hence providing adjustments to optimize process.</li>
+</ul>
+<h4>
+Baja Broadband Alamogordo, NM                
+<br>Sales Team Leader / IT 11/2011 to 05/2013 
+</h4>
+<ul>
+<li>Led a team of 10 sales partners and increased sales by 20% within the first month</li>
+<li>Spearheaded weekly operations and training towards sales goals</li>
+<li>Confidentially managed the software and programs to process transactions for customers</li>
+<li>Planned and communicated effectively with management and my sales team</li>
+<li>Helped troubleshoot clients system when it calls for it.</li>
+<li>Setup clients modems and configured wireless routers.</li>
+</ul>
+<h3>
+EDUCATION                                                                     
+</h3>
+<ul>
+<li>Bachelors of Information and communication Technology - New Mexico State University(In progress)</li>
+<li>Associates Degree in Business Marketing- New Mexico State University(03/2014)</li>
+</ul>
+
+										</article>
+
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
