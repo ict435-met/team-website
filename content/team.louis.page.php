7e5a5421793d5650a4f35d5e7333c264b2dc8e81
bpcampbe <bpcampbe@web.nmsu.edu>
2016-06-07 22:52:33 -0600

    Add Louis' resume

---
+++ content/team.louis.page.php
@@ -0,0 +1,72 @@
+<?php
+echo <<<'EOD'
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div class="container">
+						<div class="row 200%">
+							<div class="4u 12u$(medium)">
+								<div id="sidebar">
+
+									<!-- Sidebar -->
+										<section>
+																							    1805 Bellamah Drive 
+<br>66B Pillowcrest Road 
+<br>Prewitt, NM 87045 
+<br>1(505)290-7483 
+<br>louisbe@nmsu.edu
+
+											<footer>
+												<a href="#" class="button icon fa-info-circle">Contact Louis</a>
+											</footer>
+										</section>
+
+
+								</div>
+							</div>
+							<div class="8u 12u$(medium) important(medium)">
+								<div id="content">
+
+									<!-- Content -->
+										<article>
+<h2>
+Louis Bear Eagle
+</h2>
+<h3>Education</h3>
+<ul>
+<li>New Mexico State University, Las Cruces, NM	Aug 2012-Present	Working on Bachelor's in Information and Communication Technology</li>
+<li>NMSU-Grants, Grants, NM	May 2012	Earned Associate's of Applied Science</li>
+<li>NMSU-Grants, Grants, NM	Aug 2011	Earned Associate's in General Studies</li>
+<li>Chadron High School, Chadron, NE	May 2001	High School Diploma</li>
+</ul>
+<h3>Work of Experience</h3>
+<h4>Technology Services Technician	NMSU-Grants	Jan 2015-present	Grants,NM</h4>
+<p>Maintain computer labs, printers, phone system, campus network, and ITV units. Password Resets, install appropriate software for different units of campus</p>
+<h4>Computer Services Technician	NM Women's Correctional Facility CCA	July 2014-Dec 2014	Grants,NM</h4>
+<p>Maintain computers, printers, phone system. Grant Access to system. Train new employees. Correctional Officer scheduling</p>
+<h4>Information Technology Intern	NMSU-Grants	July 2012-July 2014	Grants, NM</h4>
+<p>Maintain computer labs located on campus and outreach sites.</p>
+<h4>Customer Service Associate	Walmart	May 2011-Feb 2014	Grants, NM</h4>
+<p>Worked at Customer Service desk helping customers with returns, exchanges, issues and bill payment.</p>
+<h3>Software Proficiency</h3>
+<ul>
+<li>Proficient with Microsoft Office, Server 2012, Active Directory, AutoCAD, Clonezilla, Final Cut Pro, Motion, Photoshop, Anime Studio 6, Magix Music Maker Deluxe, 3D studio Max.</li>
+<li>Experience with programming in Java, HTML, C++, Python</li>
+</ul>
+<h3>Activities/Honors</h3>
+<ul>
+<li>NMSU Grants Creative Media Department Film Club "Poco Loco Players"</li>
+<li>Skills USA club</li>
+<li>UNL Summer Institute for Promising Scholars 2001</li>
+<li>Who's Who among students in American Universities and Colleges 2010</li>
+<li>Phi Theta Kappa Honor Society</li>
+<li>NMSU Crimson Scholar</li>
+<li>NMSU-Grants Outstanding Student in Applied Science Award 2012</li>
+										</article>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+EOD;
+?>
