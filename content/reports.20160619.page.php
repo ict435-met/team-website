87ea8a304c97583554bbf414ec0b0a3125ec08b3
Brian <bpcampbe@gmail.com>
2016-06-19 20:18:46 -0700

    Change wording.

--- content/reports.20160619.page.php
+++ content/reports.20160619.page.php
@@ -1,22 +1,12 @@
-<?php
-echo <<<'EOD'
 <h2 style="text-align:left;">Report for week of 6/12 through 6/19</h2>
-                                    
-                                           
                                 <p><strong>June 15th:</strong><br>
-                                    The team met to discuss the design and being to assign tasks for the project. Brian had created a sample page using the <a href="http://www.smarty.net">Smarty template engine</a>.  He used the engine in combination with the NMSU template which can be found on <a href="https://github.com/brshaughnessy/nmsu-template-2016">GitHub</a>.  Since the engine separates the presentation (HTML/CSS) from the application(PHP), the Smarty Engine will help to keep our pages consistent with the NMSU branding.  The team agreed to use this page going forward for the project.  We then decided to assign tasks for each team member, making them responsible for a portion of the website.</p>
-                        
-    
+                                    The team met to discuss the design and to assign tasks for the project. Brian had created a sample page using the <a href="http://www.smarty.net">Smarty template engine</a>.  He used the engine in combination with the NMSU template which can be found on <a href="https://github.com/brshaughnessy/nmsu-template-2016">GitHub</a>.  Since the engine separates the presentation (HTML/CSS) from the application(PHP), the Smarty Engine will help to keep our pages consistent with the NMSU branding.  The team agreed to use this page going forward for the project.  We then decided to assign tasks for each team member, making them responsible for a portion of the website.</p>
                         <ul>
-    
                             <li><strong>Aaron Wood:</strong> Courses and links to syllabi and sample homework</li>
-                            <li><strong>Brian Campbell:</strong> Outcomes and objectives (for each major), self-study, documentation, facilities </li>
+                            <li><strong>Brian Campbell:</strong> Outcomes and objectives, self-study, documentation, facilities</li>
                             <li><strong>Felipe Parra:</strong> IAC agenda and minutes</li>
                             <li><strong>Louis Bear Eagle:</strong> Outcome measures (WEAVE documentation) and options assessments meeting minutes</li>
                             <li><strong>Rockson Opoku:</strong> Objectives and outcomes archives and faculty</li>
-    </ul>
-    
+			</ul>
     <p><strong>Sketch of task breakdown:<strong></p>
     <img src="./assets/images/2016-06-15.jpg" alt="task breakdown sketch">
-EOD;
-?>
