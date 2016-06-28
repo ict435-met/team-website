3e12922d7c5d6cc5e3e386d52688a8b99966e413
fparra <fparra@web.nmsu.edu>
2016-06-28 08:41:28 -0600

    Added weekly reort for 06/26/2016

---
+++ content/reports.20160626.page.php
@@ -0,0 +1,9 @@
+<h2>Report for week of 6/19 through 6/26</h2>
+                                <p><strong>June 26th:</strong><br>                               
+                                 The week was spent with team members working individually on their portion of the website.  We have also spent some time learning the technologies that were picked for the project.  There has been not team meeting scheduled for this week to allow members to work.  
+
+</p>
+
+    <p><strong>Next meeting:</strong><br>
+    Scheduled for June 29th at 7:00PM MST.  We need to check on progress and make any necessary adjustments.  
+</p>
