4c56a84e6cfa4df304157012e8915c3915b65d0e
fparra <fparra@web.nmsu.edu>
2016-06-28 08:51:09 -0600

    Added weekly reort for 06/26/2016

--- content/reports.page.php
+++ content/reports.page.php
@@ -11,7 +11,7 @@
 /* Make li for report links more sensible
  * Whenever a report is added an an element to this array
  */
-$links = array('20160612' => 'Week of 6/05 - 6/12', '20160619' => 'Week of 6/12 - 6/19');
+$links = array('20160612' => 'Week of 6/05 - 6/12', '20160619' => 'Week of 6/12 - 6/19','20160626' => 'Week of 6/19 - 6/26');
 foreach ($links as $r_file => $r_title) {
 	echo '<p><a class="button '.(($report == 'reports.'.$r_file.'.page.php')?'':'alt').'" href="?p=reports/'.$r_file.'">'.$r_title.'</a></p>';
 }
