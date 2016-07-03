38d595480751029566f9425a75e5b6f853e1290d
fparra <fparra@web.nmsu.edu>
2016-07-03 13:58:52 -0600

    Added weekly reort for 07/03/2016

---
+++ content/reports.20160703.page.php
@@ -0,0 +1,16 @@
+<h2>Report for week of 6/26 through 7/03</h2>
+	<p><strong>July 29th:</strong><br>
+	Aaron and Rockson met to discuss question on pull request via Adobe connect.
+	</p>
+	<p><strong>July 1st:</strong><br>
+	Brian Campbell, Rockson Opoku, and Aaron Wood met to discuss the Smarty template created by Brian. They also walked through using Git and GitHub to discuss the general configuration of the website. Additionally they discussed the custom document delivery system created by Brian. This meeting was recorded and available on the Conferences section of the MET group. 	
+	</p>
+	<p><strong>July 2nd:</strong><br>
+	Aaron shared instructions from Brian with the team to get a working copy of our website copied to our local directory on web.nmsu.edu.
+		</p>
+	
+	<p>Aaron has made some progress in adding the courses to the website and is starting to understand the courses configuration. We are still waiting on receiving the documents from Professor Kelly, so that we can begin to plug in the content needed to complete much of the website.</p>
+	<p><strong>Next meeting:</strong><br>
+July 6th at 7:00PM MST	
+	</p>
+	
