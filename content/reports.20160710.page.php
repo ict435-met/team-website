15cfd793849dcd21781b325ae29c3d32b6dd230a
fparra <fparra@web.nmsu.edu>
2016-07-10 21:13:59 -0600

    added weekly report

---
+++ content/reports.20160710.page.php
@@ -0,0 +1,13 @@
+<h2>Report for week of 7/3 through 7/10</h2>
+
+<p><strong>July 7th:</strong><br>
+Brian Campbell, Rockson Opoku, Felipe Parra, and Aaron wood met for a weekly status update.  There we discussed that we were all waiting on documents before we could do much more work on the website as the basic components of the website were complete.
+</p>
+
+<p><strong>July 10th:</strong><br>
+We received the required documentation from Professor Kelly, so we are ready to start to add content to the outer shell we have been creating. Next week we will work to add contant.
+</p>
+
+<p><strong>Next meeting:</strong><br>
+July 14th at 7:00 PM MST</p>
+
--- content/reports.page.php
+++ content/reports.page.php
@@ -11,7 +11,9 @@
 /* Make li for report links more sensible
  * Whenever a report is added an an element to this array
  */
-$links = array('20160612' => 'Week of 6/05 - 6/12', '20160619' => 'Week of 6/12 - 6/19','20160626' => 'Week of 6/19 - 6/26', '20160703' => 'Week of 6/26 - 7/3');
+$links = array( '20160612' => 'Week of 6/05 - 6/12', '20160619' => 'Week of 6/12 - 6/19',
+		'20160626' => 'Week of 6/19 - 6/26', '20160703' => 'Week of 6/26 - 7/03',
+		'20160710' => 'Week of 7/03 - 7/10');
 foreach ($links as $r_file => $r_title) {
 	echo '<p><a class="button '.(($report == 'reports.'.$r_file.'.page.php')?'':'alt').'" href="?p=reports/'.$r_file.'">'.$r_title.'</a></p>';
 }
