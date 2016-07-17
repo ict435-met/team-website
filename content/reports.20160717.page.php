3df8e1001fce508a34dc37e55aa16a28c489285a
fparra <fparra@web.nmsu.edu>
2016-07-17 16:05:50 -0600

    Adding weekly report

---
+++ content/reports.20160717.page.php
@@ -0,0 +1,9 @@
+<h2>Report for week of 7/10 – 07/17</h2>
+<p>The team received many documents needed for the Mechanical  Engineering Technology accreditation website from Professor Kelly.  Felipe was able to create the IAC meeting  notes portion of the website and upload the documents provided on 7/11.</p>
+<p><strong>July 14th:</strong><br>
+  Aaron Wood, Brian Campbell, Rockson Opoku, and Felipe Parra were able to meet to discuss our progress to develop the site since we have many of the required documents. Aaron said he would add all of  the course information this weekend.   We  could not find the &ldquo;WEAVE documents&rdquo; or the &ldquo;Options Assessment Meeting notes&rdquo;  in the files provided. Aaron is going to request those documents from Professor  Kelly. Brian mentioned he would start preparing a location for the &ldquo;WEAVE  documents&rdquo; and &ldquo;Options Assessment meeting notes&rdquo; on the website.  This was  Louis&rsquo;s section of the site, the team is beginning to worry about its  completion.  We need to verify whether or not he will need assistance since he  has missed the last few meetings. </p>
+<p><strong>July 15th:</strong><br>
+  Aaron notified the group that the courses section was set up so that files can be dropped into the files/course-syllabi  directory.  However, when adding syllabi,  he found that they were the same for each class.  He will check with Professor Kelly to see if  she wants our site to have each course or just the courses found in my degree  plan.<br>
+  </p>
+<p><strong>Next meeting:</strong><br>
+  July 21st, at 7:00 PM MST</p>
